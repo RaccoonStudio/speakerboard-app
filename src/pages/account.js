59ab890debbb43a8fa68d7f04fe34ce5c83710dd
Damien Senger <damien@raccoon.studio>
2019-06-23 16:45:03 +0200

    Add login check to account component

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,22 +1,34 @@
 import React from "react"
 
 import { Router } from "@reach/router"
+import { login, isAuthenticated, getProfile } from "../utils/auth"
 import { Link } from "gatsby"
 
-const Home = () => <p>Home</p>
+const Home = ({ user }) => {
+  return <p>Hi, {user.name ? user.name : "fella"}!</p>
+}
 const Settings = () => <p>Settings</p>
 
-const Account = () => (
-  <>
-    <nav>
-      <Link to={"/account"}>Account</Link>
-      <Link to={"/account/settings"}>Settings</Link>
-    </nav>
-    <Router>
-      <Home path={"/account"} />
-      <Settings path={"/account/settings"} />
-    </Router>
-  </>
-)
+const Account = () => {
+  if (!isAuthenticated()) {
+    login()
+    return <p>Redirecting to login…</p>
+  }
+
+  const user = getProfile()
+
+  return (
+    <>
+      <nav>
+        <Link to={"/account/"}>Account</Link>
+        <Link to={"/account/settings"}>Settings</Link>
+      </nav>
+      <Router>
+        <Home path={"/account/"} user={user} />
+        <Settings path={"/account/settings"} />
+      </Router>
+    </>
+  )
+}
 
 export default Account
