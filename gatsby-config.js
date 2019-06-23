56ecf02249068c968591218f2ce3794ca6e54109
Damien Senger <damien@raccoon.studio>
2019-06-23 15:13:22 +0200

    Customization of package.json, README.md & config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Speakerboard`,
+    description: `Speakerboard is a dashboard for speakers to manage their talk proposals and appearances.`,
+    author: `@iamhiwelo`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `speakerboard`,
+        short_name: `spk`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
